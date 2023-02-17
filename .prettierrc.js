989e5c9d12a015e79c039599a8e495094c1e65ec
cold_hailstone <cksn1993@gmail.com>
2023-02-17 19:47:10 +0900

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	singleQuote: true,
+	semi: true,
+	useTabs: true,
+	tabWidth: 4,
+	printWidth: 80,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	arrowParens: 'always',
+};
---
+++ src/views/HomeView.vue
@@ -0,0 +1,90 @@
+<template>
+	<div
+		style="
+			position: absolute;
+			display: flex;
+			align-items: center;
+			height: 100%;
+			width: 100%;
+			justify-content: center;
+			z-index: 2;
+		"
+	>
+		<h1>은하야 사랑해</h1>
+	</div>
+	<div class="back"></div>
+	<div class="heart"></div>
+</template>
+
+<script>
+export default {
+	name: 'HomeView',
+};
+</script>
+
+<style lang="scss" scoped>
+.back {
+	position: fixed;
+	padding: 0;
+	margin: 0;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: white;
+	animation-name: backdiv;
+	animation-duration: 1s;
+	animation-iteration-count: infinite;
+}
+
+.heart {
+	position: absolute;
+	margin: auto;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	background-color: pink;
+	height: 50px;
+	width: 50px;
+	transform: rotate(-45deg);
+	animation-name: beat;
+	animation-duration: 1s;
+	animation-iteration-count: infinite;
+}
+.heart:after {
+	background-color: pink;
+	content: '';
+	border-radius: 50%;
+	position: absolute;
+	width: 50px;
+	height: 50px;
+	top: 0px;
+	left: 25px;
+}
+.heart:before {
+	background-color: pink;
+	content: '';
+	border-radius: 50%;
+	position: absolute;
+	width: 50px;
+	height: 50px;
+	top: -25px;
+	left: 0px;
+}
+
+@keyframes backdiv {
+	50% {
+		background: #ffe6f2;
+	}
+}
+
+@keyframes beat {
+	0% {
+		transform: scale(1) rotate(-45deg);
+	}
+	50% {
+		transform: scale(0.6) rotate(-45deg);
+	}
+}
+</style>
